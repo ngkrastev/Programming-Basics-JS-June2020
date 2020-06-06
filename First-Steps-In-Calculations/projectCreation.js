f8f27431167f61aefe4bf5aa07fc081bdbda5f87
Nikolay Krastev <65648105+ngkrastev@users.noreply.github.com>
2020-06-06 21:48:41 +0300

    Create projectCreation.js

---
+++ First-Steps-In-Calculations/projectCreation.js
@@ -0,0 +1,8 @@
+function projectCreation(architect, hours) {
+    let name = architect;
+    let project = hours;
+    let time = 3 * project;
+    console.log(`The architect ${name} will need ${time} hours to complete ${project} project/s.`)
+}
+
+projectCreation("George", 4)
