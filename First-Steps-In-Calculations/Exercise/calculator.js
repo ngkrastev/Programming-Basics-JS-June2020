8220657e58f7e81901e205b80525f522c3f3509b
Nikolay Krastev <65648105+ngkrastev@users.noreply.github.com>
2020-06-07 12:14:24 +0300

    Update calculator.js

--- First-Steps-In-Calculations/Exercise/calculator.js
+++ First-Steps-In-Calculations/Exercise/calculator.js
@@ -1,29 +1,12 @@
-// function calculator(deposit, time, yearpercent) {
-//     deposit = Number(deposit);
-//     time = Number(time);
-//     yearpercent = Number(yearpercent);
-//     let forYear = deposit * yearpercent / 100;
-//     let forMonth = forYear / 12;
-//     let result = deposit + time * forMonth;
-
-//     console.log(result);
-// }
-
-// calculator("200", "3", "5.7");
-// calculator("2350", "6", "7");
-
-
-// сума = депозирана сума  + срок на депозита * ((депозирана сума * годишен лихвен процент ) / 12)
-
-
 function calculator(deposit, time, yearpercent) {
-    deposit = Number(deposit);
-    time = Number(time);
-    yearpercent = Number(yearpercent);
-    let forMonth = deposit * yearpercent / 100 / 12;
-    let result = deposit + time * forMonth;
+     deposit = Number(deposit);
+     time = Number(time);
+     yearpercent = Number(yearpercent);
+     let forYear = deposit * yearpercent / 100;
+     let forMonth = forYear / 12;
+     let result = deposit + time * forMonth;
 
-    console.log(result);
+     console.log(result);
 }
 
 calculator("200", "3", "5.7");
